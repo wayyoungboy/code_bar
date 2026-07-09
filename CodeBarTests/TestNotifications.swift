35ac196f9a1b0b374147ef7c9af9f347946ef77e
xuyan.wxy <xuyan.wxy@oceanbase.com>
2026-07-09 20:21:34 +0800

    Fix behavior test notification definitions

--- CodeBarTests/TestNotifications.swift
+++ CodeBarTests/TestNotifications.swift
@@ -3,4 +3,5 @@ import Foundation
 extension Notification.Name {
     static let showSettings = Notification.Name("showSettings")
     static let moduleStatusItemsChanged = Notification.Name("moduleStatusItemsChanged")
+    static let usageDataUpdated = Notification.Name("usageDataUpdated")
 }
